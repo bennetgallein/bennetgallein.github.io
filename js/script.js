dcb9b09059c31f1c44b8d11f7b61c7394642229f
ScorpiaWeb <info@team-scorpia.de>
2016-11-22 20:19:12 +0100

    Added r.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	var preloader = 100;
+   $(".container").delay(preloader).fadeIn(400);
+   console.log("Hello World");
+});
---
+++ projects/r.js/js/r.js
@@ -0,0 +1,27 @@
+ var rjs =  new function () {
+
+	this.randomInt = function(min, max) {
+		return Math.floor(Math.random() * (max-min)) + min;
+	};
+	this.randomDec = function(min, max, decimals) {
+		return (Math.random() * (max-min) + min).toFixed(decimals || 2); 
+	};
+	this.randomList = function(list) {
+		var i =  list[rjs.randomInt(0, list.length)];
+		return i;
+	};
+	this.randomPercent = function(decimals) {
+		return rjs.randomDec(0, 100, decimals);
+	};
+	this.randomString = function(length) {
+		var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		var text = "";
+		for (var i = 0; i < length; i++) {
+			text += chars.charAt(Math.random() * chars.length);
+		} 
+		return text;
+	};
+	this.randomBoolean = function() {
+		return rndm = Math.random() >= 0.5;
+	}
+};
